e4ec066a44142e60c588697d958f4a4cccdb747a
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-11 18:10:45 -0300

    config: prisma and next auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,9 +1,24 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '../database'
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: '/auth',
+    error: '/auth',
+    signOut: '/auth',
+    verifyRequest: '/auth',
+    newUser: '/auth',
+  },
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 })
