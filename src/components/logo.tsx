a5b78dad77c0f741635e974c652e0f7e85e6c274
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-14 08:27:15 -0300

    feat: logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import { RocketIcon } from '@radix-ui/react-icons'
+
+export default function Logo() {
+  return (
+    <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary">
+      <RocketIcon className=" h-3 w-3 text-primary-foreground" />
+    </div>
+  )
+}
