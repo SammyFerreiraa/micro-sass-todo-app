919d9355020cfe7b8f7c7897c86191668e73de9e
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-17 18:52:02 -0300

    feat: add upsert button

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,14 +1,24 @@
 import { DashboardPage } from '@/components/dashboard/page'
 import { TodoDataTable } from './_components/todo-data-table'
+import { Button } from '@/components/ui/button'
+import { TodoUpsertSheet } from './_components/todo-upsert-sheet'
+import { PlusIcon } from '@radix-ui/react-icons'
 
 export default async function AppPage() {
   return (
     <DashboardPage>
       <DashboardPage.Header>
         <DashboardPage.Header.Title>Tarefas</DashboardPage.Header.Title>
+        <DashboardPage.Header.Nav>
+          <TodoUpsertSheet>
+            <Button variant={'outline'} size={'sm'}>
+              <PlusIcon className="mr-2 h-4 w-4" />
+              Add Todo
+            </Button>
+          </TodoUpsertSheet>
+        </DashboardPage.Header.Nav>
       </DashboardPage.Header>
       <DashboardPage.Main>
-        <h1>Tarefas</h1>
         <TodoDataTable />
       </DashboardPage.Main>
     </DashboardPage>
